179a261131154129519ab2d1107720690c53c4dc
YKosArt <yurii.kostyuk.ua@gmail.com>
2025-09-29 22:27:31 +0200

    ок

--- obj/Debug/net9.0/EventEaseF.AssemblyInfo.cs
+++ obj/Debug/net9.0/EventEaseF.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEaseF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea1c93e208179c792e8f43ab9a43baba056e62ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEaseF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEaseF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
